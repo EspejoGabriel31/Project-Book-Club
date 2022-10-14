3f9f576f259015d4e30d683149dc0eda6eff7237
unknown <espejo.gabriel779@gmail.com>
2022-10-14 10:35:24 -0700

    added comments

--- server/models/book.js
+++ server/models/book.js
@@ -1,30 +1,31 @@
 'use strict'
 const { 
-    Model 
+    Model                                           //import Model Superclass from sequelize
 } = require('sequelize')
 
-module.exports = (sequelize, DataTypes) => {
+module.exports = (sequelize, DataTypes) => {        //start Book Model
     //posts on books
-    class Book extends Model {
-        static associate({ Post }){
-            Book.hasMany(Post, {foreignKey: 'book_id', as: 'posts'})
+    class Book extends Model {                      //Book inheirits Model's base traits 
+        static associate({ Post }){                     //Book is associated with the Post Model...
+            Book.hasMany(Post, {foreignKey: 'book_id', as: 'posts'})        //...in a One-to-Many relationship
         }
     }
 
-    Book.init({
-        bookId: {
+    Book.init({                                     //Setting Book Model's attributes
+        bookId: {                                   //Book Id automatically generates and is set to the primary key
             type: DataTypes.SMALLINT,
             primaryKey: true,
             autoIncrement: true
         },
-        title: DataTypes.STRING,
-        author: DataTypes.STRING,
-        pic: DataTypes.STRING,
-        published: DataTypes.INTEGER
+        title: DataTypes.STRING,                    //Title of Book
+        author: DataTypes.STRING,                   //Author of Book
+        pic: DataTypes.STRING,                      //Picture of Book
+        published: DataTypes.INTEGER,                //Published Date of Book
+        //Add More Attributes
     }, {
-        sequelize,
+        sequelize,                                  //misc Sequelize stuff
         underscored: true,
         modelName: 'Book',
     })
-    return Book
+    return Book                                     //Return Book
 }
--- server/models/index.js
+++ server/models/index.js
@@ -43,3 +43,29 @@ db.sequelize = sequelize
 db.Sequelize = Sequelize
 
 module.exports = db
+
+// require('dotenv').config()
+
+// module.exports = {
+//     "development": {
+//         "username": process.env.DB_USERNAME,
+//         "password": process.env.DB_PASSWORD,
+//         "database": process.env.DB_DATABASE,
+//         "host": "127.0.0.1",
+//         "dialect": "postgres"
+//     },
+//     "test": {
+//         "username": "root",
+//         "password": "null",
+//         "database": "database_test",
+//         "host": "127.0.0.1",
+//         "dialect": "mysql"
+//     },
+//     "production": {
+//         "username": "root",
+//         "password": "null",
+//         "database": "database_production",
+//         "host": "127.0.0.1",
+//         "dialect": "mysql"
+//     }
+// }
