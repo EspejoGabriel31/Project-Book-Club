6b38b668fa629d16e802ae78a3fe3053baa30d22
unknown <espejo.gabriel779@gmail.com>
2022-10-14 17:51:40 -0700

    moved old files. added comments to User Model

--- server/models/user.js
+++ server/models/user.js
@@ -1,28 +1,31 @@
 'use strict'
 const {
-    Model
+    Model                   //import Model Superclass from sequelize
 } = require('sequelize')
 
-module.exports = (sequelize, DataTypes) => {
-    class User extends Model {
-        static associate({ Post }){
+module.exports = (sequelize, DataTypes) => {        //start and export User Model
+    
+    class User extends Model {                      //Create User Class from Sequelize Model
+        // static associate({ Post }){
 
-            //user's posts
-            User.hasMany(Post, { 
-                as: 'author', 
-                foreignKey: 'author_id'
-            })
-        }
+        //     //user's posts                       //Associations to be added later
+        //     User.hasMany(Post, { 
+        //         as: 'author', 
+        //         foreignKey: 'author_id'
+        //     })
+        // }
     }
-    User.init({
-        userId: {
+
+    User.init({                                     //Set User Model's attributes
+        userId: {                                   //User Id automatically generated and set as Primary Key
             type: DataTypes.SMALLINT,
             primaryKey: true,
             autoIncrement: true
         },
-        firstName: DataTypes.STRING,
-        lastName: DataTypes.STRING,
-        email: DataTypes.STRING
+        firstName: DataTypes.STRING,                //User's first name
+        lastName: DataTypes.STRING,                 //User's last name
+        email: DataTypes.STRING,                    //User's email
+        passwordDigest: DataTypes.STRING            //Digest for User's password
     }, {
         sequelize,
         underscored: true,
