28502478895221e23eea366adddec5438d5d88c9
unknown <espejo.gabriel779@gmail.com>
2022-10-15 23:30:42 -0700

    added comments for modifying auto generated models and migrations

--- server/migrations/20221016055634-create-book.js
+++ server/migrations/20221016055634-create-book.js
@@ -2,15 +2,15 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('books', {
-      book_id: {
+    await queryInterface.createTable('books', { //table name to lowercase
+      book_id: {                    //change to Model's id and delete the other id attribute
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
       genre: {
-        type: Sequelize.TEXT,
+        type: Sequelize.TEXT,     //Add allowNull: false to everything that has allowNulls in the Model
         allowNull: false
       },
       book_name: {
@@ -36,6 +36,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('books');
+    await queryInterface.dropTable('books');    //change model name to all lowercase
   }
 };
--- server/models/book.js
+++ server/models/book.js
@@ -13,11 +13,19 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
+
+  /* 
+    to expand model:
+    -for each attribute, place everything after the attribute name in {}
+    -add 'type:' for each DataTypes
+    -add 'allowNull: false' to each attribute
+  */
+
   Book.init({
     book_id: {
       type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
+      primaryKey: true,               //Add
+      autoIncrement: true             //Add
     },
     genre: {
       type: DataTypes.TEXT,
@@ -38,8 +46,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Book',
-    tableName: 'books',
-    timestamps: false
+    tableName: 'books', //add table name as lowercase model name plural
+    timestamps: false   //add timestamps: false
   });
   return Book;
 };
