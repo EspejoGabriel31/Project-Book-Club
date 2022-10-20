270e2599654b11ab4225820c40c2e650f1ef1688
Gabriel Espejo <espejo.gabriel779@gmail.com>
2022-10-20 16:49:50 -0700

    cleaned code

--- client/src/components/PostContainer.js
+++ client/src/components/PostContainer.js
@@ -1,14 +1,10 @@
-function PostContainer({ post
-    , onDelete 
-}) {
-    // console.log("post: ", post.user)
+function PostContainer({ post, onDelete }) {
     return (
         <div>
             <h3>
                 <strong>{post.user.first_name} {post.user.last_name}</strong>
             </h3>
             <p>{post.content}</p>
-            {/* <h4>Rating: {post.stars}</h4> */}
             <button className="btn btn-danger" onClick={onDelete} >
                 Delete Post
             </button>
