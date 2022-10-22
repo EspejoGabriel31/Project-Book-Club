a4c9f093a579dbf1b99953abe2b4e40812d6ee09
Gabriel Espejo <espejo.gabriel779@gmail.com>
2022-10-22 10:04:18 -0700

    ...

---
+++ client/src/components/contexts/CurrentUser.js
@@ -0,0 +1,28 @@
+import { createContext, useState, useEffect} from "react";
+
+export const CurrentUser = createContext()
+
+function CurrentUserProvider({children}){
+    const [currentUser, setCurrentUser] = useState(null)
+
+    useEffect(() => {
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:7000/authentication/profile', {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
+    window.setCurrentUser = setCurrentUser
+    return(
+        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
+            {children}
+        </CurrentUser.Provider>
+    )
+}
+
+export default CurrentUserProvider
