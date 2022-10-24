a7858eb37e1eedf58035a4a248482ea82d559103
Gabriel Espejo <espejo.gabriel779@gmail.com>
2022-10-23 17:02:22 -0700

    production database environment variables

--- server/config/config.js
+++ server/config/config.js
@@ -17,10 +17,11 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": "127.0.0.1",
+    "username": process.env.RDS_USERNAME,
+    "password": process.env.RDS_PASSWORD,
+    "database": process.env.RDS_DB_NAME,
+    "host": process.env.RDS_HOSTNAME,
+    "port": process.env.RDS_PORT,
     "dialect": "postgres"
   }
 }
